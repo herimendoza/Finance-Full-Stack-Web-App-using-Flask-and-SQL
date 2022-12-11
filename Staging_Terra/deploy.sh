9b0e57ceee5415d29ff5ede7b69ad3ec9a0be632
bmol5 <108373176+bmol5@users.noreply.github.com>
2022-12-11 15:03:17 -0500

    installing dependencies file

--- Staging_Terra/deploy.sh
+++ Staging_Terra/deploy.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-sudo apt update 
+sudo apt update
+sudo apt -y upgrade
 sudo apt -y install git
 sleep 3
 sudo apt -y install python3-pip
 sleep 3
-git clone https://github.com/bmol5/Finance-Full-Stack-Web-App-using-Flask-and-SQL Finance
+git clone -b brayan https://github.com/bmol5/Finance-Full-Stack-Web-App-using-Flask-and-SQL Finance
 sleep 2
 cd Finance/
 sudo apt-get -y install mysql-server
@@ -13,4 +14,4 @@ sudo apt-get -y install libmysqlclient-dev
 pip3 install flask-mysqldb
 pip install -r requirements.txt
 pip install gunicorn
-python3 -m gunicorn -w 4 application:app -b 0.0.0.0 --daemon
+#python3 -m gunicorn -w 4 application:app -b 0.0.0.0 --daemon
