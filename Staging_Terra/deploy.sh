0da19272753f1687a42f10e024614e0735239ab4
bmol5 <108373176+bmol5@users.noreply.github.com>
2022-12-11 22:59:59 -0500

    Update deploy.sh

--- Staging_Terra/deploy.sh
+++ Staging_Terra/deploy.sh
@@ -5,13 +5,14 @@ sudo apt -y upgrade
 sudo apt -y install git
 sleep 3
 sudo apt -y install python3-pip
+pip install --upgrade pip
 sleep 3
-git clone -b brayan https://github.com/bmol5/Finance-Full-Stack-Web-App-using-Flask-and-SQL Finance
+git clone https://github.com/bmol5/Finance-Full-Stack-Web-App-using-Flask-and-SQL Finance
 sleep 2
 cd Finance/
 sudo apt-get -y install mysql-server
 sudo apt-get -y install libmysqlclient-dev
 pip3 install flask-mysqldb
 pip install -r requirements.txt
-pip install gunicorn
-python3 -m gunicorn -w 4 application:app -b 0.0.0.0 --daemon
+export FLASK_APP=application
+flask run --host=0.0.0.0
