4ad455f5e36cdfcff4e425f94e503a20aaceecb8
bmol5 <108373176+bmol5@users.noreply.github.com>
2022-12-12 20:47:05 -0500

    Update deploy.sh

--- Staging_Terra/deploy.sh
+++ Staging_Terra/deploy.sh
@@ -15,10 +15,5 @@ sudo apt-get -y install libmysqlclient-dev
 pip3 install flask-mysqldb
 pip install -r requirements.txt
 sed '50!d' < application.py
-cat outputs.txt > .env
-sql_uri=$(cat outputs.txt | sed '2!d')
-echo $sql_uri
-export DB_URI=$(echo $sql_uri)
-source .env
 export FLASK_APP=application
 flask run --host=0.0.0.0
