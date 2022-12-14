aa98384307b317bd75ab7300be900f3ff97c88df
bmol5 <108373176+bmol5@users.noreply.github.com>
2022-12-13 21:24:06 -0500

    Update main.tf

--- Staging_Terra/main.tf
+++ Staging_Terra/main.tf
@@ -41,7 +41,7 @@ resource "aws_instance" "Web_Server" {
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.subnet1.id
   vpc_security_group_ids = [aws_security_group.web_ssh.id]
-  user_data = "${file("deploy.sh")}"
+  user_data = "${file("deploy1.sh")}"
   
   key_name = "KuraG5Key"
  
